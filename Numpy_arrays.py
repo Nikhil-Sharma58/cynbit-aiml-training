a2009414107a9878d480d3060e86f6e53e486c2b
Nikhil-Sharma58 <nikhil.sharma977999@gmail.com>
2026-06-22 11:12:10 +0530

    NumPy Basics Task

---
+++ Numpy_arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5])
+arr2 = np.array([10,20,30,40,50])
+print("Array 1:",arr1)
+print("Array 2:",arr2)
+# Basic operations
+print("\nAddition:",arr1 + arr2)
+print("Subtraction:",arr2 - arr1)
+print("Multiplication:",arr1 * arr2)
+print("Division:",arr2 / arr1)
